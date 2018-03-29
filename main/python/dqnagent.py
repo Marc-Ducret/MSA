d8d3c1a31c3336754937e5786a439ce31cdb3d7f
VengeurK <vengeurk@gmail.com>
2018-03-28 17:06:37 -0700

    cleaning

--- main/python/dqnagent.py
+++ main/python/dqnagent.py
@@ -51,7 +51,7 @@ class DQNAgent:
             X = np.concatenate((X, state))
             Y = np.concatenate((Y, target_f))
 
-        self.model.fit(np.array(X), np.array(Y), epochs=1, verbose=0)
+        self.model.fit(X, Y, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -60,35 +60,3 @@ class DQNAgent:
 
     def save(self, name):
         self.model.save_weights(name)
-
-
-if __name__ == "__main__":
-    EPISODES = 1000
-
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
-    done = False
-    batch_size = 32
-
-    for e in range(EPISODES):
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
-        for time in range(500):
-            # env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            reward = reward if not done else -10
-            next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
-            if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
-                break
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
