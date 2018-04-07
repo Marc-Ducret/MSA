fba1c8adeba05aa5ffd24b67a53736d2e9ce4d25
Marc Ducret <vengeurk@gmail.com>
2018-04-06 18:52:58 -0700

    push competitive environment

---
+++ main/java/edu/usc/thevillagers/serversideagent/env/EnvironmentPush.java
@@ -0,0 +1,62 @@
+package edu.usc.thevillagers.serversideagent.env;
+
+import edu.usc.thevillagers.serversideagent.agent.Agent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldServer;
+
+public class EnvironmentPush extends Environment {
+
+	protected BlockPos ref;
+	
+	private Agent agentA;
+	private Agent agentB;
+	
+	public EnvironmentPush() {
+		super("Push", 4, 2);
+	}
+	
+	@Override
+	public void init(WorldServer world) {
+		super.init(world);
+		ref = getOrigin();
+	}
+	
+	@Override
+	public void newAgent(Agent a) {
+		super.newAgent(a);
+		if(agentA == null) agentA = a;
+		else if(agentB == null) {
+			agentB = a;
+			agentB.envData = agentA;
+			agentA.envData = agentB;
+		}
+	}
+
+	@Override
+	public void encodeObservation(Agent agent, float[] stateVector) {
+		stateVector[0] = (float) (agent.entity.posX - ref.getX());
+		stateVector[1] = (float) (agent.entity.posZ - ref.getZ());
+		Agent opponent = opponent(agent);
+		stateVector[2] = (float) (opponent.entity.posX - ref.getX());
+		stateVector[3] = (float) (opponent.entity.posZ - ref.getZ());
+	}
+
+	@Override
+	public void decodeAction(Agent agent, float[] actionVector) {
+		agent.entity.actionState.forward = actionVector[0];
+		agent.entity.actionState.strafe = actionVector[1];
+	}
+
+	@Override
+	protected void stepAgent(Agent agent) throws Exception {
+		if(agent.entity.posY < ref.getY() - .01F) {
+			done = true;
+			agent.reward = -10;
+			opponent(agent).reward = 10;
+		}
+	}
+	
+	private Agent opponent(Agent a) {
+		return (Agent) a.envData;
+	}
+}
