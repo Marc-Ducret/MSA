6d3518bbcb0a20c4953810f5964a2865051cdda5
Marc Ducret <vengeurk@gmail.com>
2018-05-21 14:58:17 -0700

    no baseline dependency (to run on gpu...)

--- main/python/single_env_agent.py
+++ main/python/single_env_agent.py
@@ -2,7 +2,7 @@ from minecraft_environment import *
 import argparse
 import time
 import random
-import baselines.logger as logger
+#import baselines.logger as logger
 
 def _run(args, run):
     env = MinecraftEnv(args.env_type, args.env_id)
@@ -11,9 +11,9 @@ def _run(args, run):
     env.close()
 
 def run_agent(run, params={}):
-    logger.Logger.CURRENT = logger.Logger('tmp/logs/', [
-        logger.TensorBoardOutputFormat('tmp/logs/run_%i_%i' % (int(time.time() * 1e3), int(random.random() * 1e6))),
-        logger.HumanOutputFormat(sys.stdout)])
+    #logger.Logger.CURRENT = logger.Logger('tmp/logs/', [
+    #    logger.TensorBoardOutputFormat('tmp/logs/run_%i_%i' % (int(time.time() * 1e3), int(random.random() * 1e6))),
+    #    logger.HumanOutputFormat(sys.stdout)])
     parser = argparse.ArgumentParser()
     parser.add_argument('env_type', action='store')
     params.update({'env_id': ""})
