545423e5cb25afa22fd57748edefc6c782bfa839
Marc Ducret <vengeurk@gmail.com>
2018-07-30 17:15:17 -0700

    can specify custom state initializer

--- main/python/agent_backplay.py
+++ main/python/agent_backplay.py
@@ -3,6 +3,9 @@ from minecraft.environment import *
 import random
 import subprocess
 import time
+import threading
+import sys
+import torch as th
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,21 +16,40 @@ def main():
     args = parser.parse_args()
 
     agents_ready = False
+    steps_elapsed = 0
+    steps_limit = 60
 
     def step(done):
-        return (MinecraftController.LOAD, random.randint(args.t_min, args.t_max)) if done and agents_ready else (MinecraftController.WAIT, None)
+        if not agents_ready:
+            return (MinecraftController.WAIT, None)
+        if not done:
+            nonlocal steps_elapsed
+            steps_elapsed += 1
+            if steps_elapsed >= steps_limit:
+                done = True
+                steps_elapsed = 0
+        return (MinecraftController.LOAD, random.randint(args.t_min, args.t_max)) if done else (MinecraftController.WAIT, None)
 
     c = MinecraftController("Pattern", step, "part1")
     c.start()
 
     command = "%s --env-name mc.%s.%s" % (args.partial_command, c.env_type, c.env_id)
-    processes = [subprocess.Popen(command.split()) for _ in range(1)]
+    sys.argv = command.split()
+    thread = threading.Thread(target=rl_main)
+    thread.start()
 
     time.sleep(2)
     agents_ready = True
 
-    for p in processes:
-        p.wait()
+    thread.join()
+
+def states_initializer(policy, actor_id):
+    #print('reset', actor_id)
+    return th.zeros(policy.state_size)
+
+def rl_main():
+    import rl.main
+    rl.main.main(states_initializer=states_initializer)
 
 if __name__ == '__main__':
     main()
