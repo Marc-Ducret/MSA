6b4cfffb754e99838359e549a23ca5404a2558b0
liychen <liychen@student.ethz.ch>
2018-08-07 17:25:35 -0700

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,30 @@
 cd ..
 
-wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.2.2611/forge-1.12.2-14.23.2.2611-mdk.zip -O ../forge.zip
+sys="$( uname -s )"
+
+case "$sys" in
+	Linux*)
+		wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.2.2611/forge-1.12.2-14.23.2.2611-mdk.zip -O ../forge.zip
+
+		;;
+	Darwin*)
+		curl http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.2.2611/forge-1.12.2-14.23.2.2611-mdk.zip -o ../forge.zip
+		;;
+	*)
+esac
 unzip ../forge.zip -d ../
 rm ../forge.zip
 
 mkdir libs
-wget http://svwh.dl.sourceforge.net/project/nujan/Nujan-1.4.2.jar -O libs/Nujan-1.4.2.jar
+case "$sys" in 
+	Linux*)
+		wget http://svwh.dl.sourceforge.net/project/nujan/Nujan-1.4.2.jar -O libs/Nujan-1.4.2.jar
+		;;
+	Darwin*)
+		curl http://svwh.dl.sourceforge.net/project/nujan/Nujan-1.4.2.jar -o libs/Nujan-1.4.2.jar
+		;;
+	*)
+esac
 
 cp -r ../gradle .
 cp ../gradlew .
